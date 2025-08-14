a003b4685ba5e9b77d3e33aa5b9896ee836f5dd0
yl-yue <yl-yue@qq.com>
2025-08-14 14:37:02 +0800

    2

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -21,7 +21,7 @@ export const en = defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/yaoqing-open/yaoqing-docs/edit/main/docs/:path',
       text: 'Edit this page on GitHub'
     },
 
--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -21,7 +21,7 @@ export const zh = defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/yaoqing-open/yaoqing-docs/edit/main/docs/:path',
       text: '在 GitHub 上编辑此页面'
     },
 
