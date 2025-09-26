943587463fc65c969c15332d68373b5f254fcdea
wf <wufanwind@qq.com>
2025-09-26 14:03:27 +0800

    整理文档

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -122,8 +122,8 @@ function sidebarProfessional(): DefaultTheme.SidebarItem[] {
       items: [
         { text: '数据接入', link: '4数据接入/数据接入' },
 		{ text: '数据类型映射', link: '4数据接入/数据类型映射' },
-		{ text: 'MySQL 接入', link: '4数据接入/MySQL 接入' },
-		{ text: 'TiDB Cloud 接入', link: '4数据接入/TiDB Cloud 接入' },
+		{ text: 'MySQL 接入说明', link: '4数据接入/MySQL 接入' },
+		{ text: 'TiDB Cloud 接入说明', link: '4数据接入/TiDB Cloud 接入' },
 		{ text: '常见问题', link: '4数据接入/常见问题' },
       ]
     },
@@ -132,8 +132,8 @@ function sidebarProfessional(): DefaultTheme.SidebarItem[] {
       collapsed: false,
       items: [
         { text: '数据清洗', link: '5数据清洗/数据清洗' },
-        { text: '清洗规范', link: '5数据清洗/清洗规范' },
-        { text: '爻擎 SQL', link: '5数据清洗/爻擎 SQL' },
+        { text: '爻擎 SQL 规范', link: '5数据清洗/爻擎 SQL 规范' },
+        { text: '爻擎 SQL 函数', link: '5数据清洗/爻擎 SQL 函数' },
         { text: '快捷键', link: '5数据清洗/快捷键' },
       ]
     },
